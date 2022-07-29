5fd5683037907fc44623218c628d053db669155e
Dikici <Dikici@LAPTOP-T4DQ30N0>
2022-07-29 16:47:02 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+
+            int[] sayilar1 = new int[] { 1, 2 };
+            int[] sayilar2 = new int[] { 3, 4 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 500;
+            Console.WriteLine(sayilar1[0]);
+        }
+    }
+}
