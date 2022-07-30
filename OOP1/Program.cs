14ae84adeec6d17e62add7d8d93fbf3d9bd9eaa6
İsmet Dikici <ismetdikici@hotmail.com>
2022-07-30 13:56:52 +0300

    BootCamp 4

---
+++ OOP1/Program.cs
@@ -0,0 +1,23 @@
+﻿using OOP1;
+using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ID = 1;
+            product1.CategoryID = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { ID = 2, CategoryID = 5, ProductName = "Kalem", UnitsInStock = 5, UnitPrice = 35 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
